9f0a188182e4aac7ffb17c559b8f8b8d3edd0cc0
Jayanthi Sashank <sashihankler19@gmail.com>
2025-06-10 11:27:30 +0530

    Time: 510 ms (93.58%) | Memory: 0B (100.00%) - LeetSync

--- PYTHON/Arrays/TwoPointers/EasyLevel/1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ PYTHON/Arrays/TwoPointers/EasyLevel/1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,20 +1,9 @@
-/* Write your T-SQL query statement below */
-WITH ranked_employees AS (
-    SELECT 
-        employee_id,
-        department_id,
-        primary_flag,
-        COUNT(*) OVER(PARTITION BY employee_id) AS rank_count
-    FROM employee
-)
-SELECT 
-    distinct employee_id,
-    CASE 
-        WHEN rank_count = 1 THEN department_id
-        WHEN rank_count > 1 THEN (
-            SELECT TOP 1 department_id 
-            FROM employee e2 
-            WHERE e2.employee_id = e1.employee_id AND e2.primary_flag = 'Y'
-        )
-    END AS department_id
-FROM ranked_employees e1;
+# Write your MySQL query statement below
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag='Y' OR 
+    employee_id in
+    (SELECT employee_id
+    FROM Employee
+    Group by employee_id
+    having count(employee_id)=1)
