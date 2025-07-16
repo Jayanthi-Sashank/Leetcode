8f1f9f089ac32d3415f42045febac62ecbff5f93
Jayanthi Sashank <sashihankler19@gmail.com>
2025-07-16 17:18:42 +0530

    Time: 2 ms (90.84%) | Memory: 17.8 MB (64.17%) - LeetSync

--- PYTHON/Arrays/TwoPointers/EasyLevel/9-palindrome-number/palindrome-number.py
+++ PYTHON/Arrays/TwoPointers/EasyLevel/9-palindrome-number/palindrome-number.py
@@ -1,15 +1,5 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        x=str(x)
-        left=0
-        right=len(x)-1
-        while(left<right):
-            if x[left]!=x[right]:
-                return False
-            left=left+1
-            right=right-1
-        return True
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False  # negative numbers are not palindromes
+        return str(x) == str(x)[::-1]
