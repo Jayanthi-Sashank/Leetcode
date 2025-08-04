b74104d547304f0be530c92836d49b28df60fb0b
Jayanthi Sashank <sashihankler19@gmail.com>
2025-08-05 04:46:55 +0530

    Time: 749 ms (30.76%) | Memory: 0B (100.00%) - LeetSync

--- PYTHON/Arrays/TwoPointers/EasyLevel/577-employee-bonus/employee-bonus.sql
+++ PYTHON/Arrays/TwoPointers/EasyLevel/577-employee-bonus/employee-bonus.sql
@@ -1,8 +1,7 @@
 /* Write your T-SQL query statement below */
-select e1.name,b1.bonus
-from Employee e1
- left join Bonus b1
-on b1.empId=e1.empID
-where b1.bonus<1000 or b1.bonus is null
-
+select name,bonus 
+from Employee e
+left join Bonus b
+on e.empId=b.empId
+where bonus<1000 or bonus is null
 
