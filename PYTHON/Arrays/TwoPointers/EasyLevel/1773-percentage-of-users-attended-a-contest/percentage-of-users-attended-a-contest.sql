7a05cd60d88bd3c7e2cbcf026bb2009e91a5fc75
Jayanthi Sashank <sashihankler19@gmail.com>
2025-08-05 16:37:45 +0530

    Time: 2814 ms (47.75%) | Memory: 0B (100.00%) - LeetSync

--- PYTHON/Arrays/TwoPointers/EasyLevel/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ PYTHON/Arrays/TwoPointers/EasyLevel/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,7 +1,8 @@
-# Write your MySQL query statement below
-select contest_id,ROUND((COUNT(DISTINCT r.user_id) / (SELECT COUNT(user_id) FROM Users)) * 100, 2)as percentage
+/* Write your T-SQL query statement below */
+
+select contest_id,ROUND((COUNT(DISTINCT r.user_id)*100 / (SELECT COUNT(user_id)*1.0 FROM Users)), 2)as percentage
 from Register R
 join Users U
 on U.user_id=R.user_id
 group by contest_id
-order by percentage desc,contest_id asc
+order by percentage desc, contest_id asc
