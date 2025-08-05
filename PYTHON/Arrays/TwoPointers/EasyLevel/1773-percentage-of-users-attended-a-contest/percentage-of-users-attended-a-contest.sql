e5486b8943a19882539f760b3247b0eedc6894fb
Jayanthi Sashank <sashihankler19@gmail.com>
2025-08-05 16:35:23 +0530

    Time: 1208 ms (15.28%) | Memory: 0B (100.00%) - LeetSync

--- PYTHON/Arrays/TwoPointers/EasyLevel/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ PYTHON/Arrays/TwoPointers/EasyLevel/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-SELECT 
-    contest_id, 
-    ROUND(COUNT(DISTINCT user_id) * 100 / (SELECT COUNT(user_id) FROM Users), 2) AS percentage
-FROM Register
-GROUP BY contest_id
-ORDER BY percentage DESC, contest_id;
+select contest_id,ROUND((COUNT(DISTINCT r.user_id) / (SELECT COUNT(user_id) FROM Users)) * 100, 2)as percentage
+from Register R
+join Users U
+on U.user_id=R.user_id
+group by contest_id
+order by percentage desc,contest_id asc
