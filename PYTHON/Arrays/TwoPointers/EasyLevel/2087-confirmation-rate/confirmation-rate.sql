e2d7712915a6d4326273dc17ceae8eaa21a21c58
Jayanthi Sashank <sashihankler19@gmail.com>
2025-08-06 21:50:22 +0530

    Time: 523 ms (39.01%) | Memory: 0B (100.00%) - LeetSync

--- PYTHON/Arrays/TwoPointers/EasyLevel/2087-confirmation-rate/confirmation-rate.sql
+++ PYTHON/Arrays/TwoPointers/EasyLevel/2087-confirmation-rate/confirmation-rate.sql
@@ -1,7 +1,6 @@
 /* Write your T-SQL query statement below */
-select s.user_id,round(avg(case when action ='confirmed' then 1.00 else 0 end ),2) as confirmation_rate
-from signups s
-left join confirmations c
-on s.user_id=c.user_id
-group by s.user_id
-
+select S.user_id,round(sum(case when action='confirmed' then 1 else 0 end)*1.0/count(*),2)as confirmation_rate
+from Signups S
+left join Confirmations C
+on S.user_id=C.user_id
+group by S.user_id
