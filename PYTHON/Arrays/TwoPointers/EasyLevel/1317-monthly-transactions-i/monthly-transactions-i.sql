457c0d60fc514150f29b4e5bf9ad9b6176d9b2e5
Jayanthi Sashank <sashihankler19@gmail.com>
2025-08-06 20:33:27 +0530

    Time: 1761 ms (29.15%) | Memory: 0B (100.00%) - LeetSync

--- PYTHON/Arrays/TwoPointers/EasyLevel/1317-monthly-transactions-i/monthly-transactions-i.sql
+++ PYTHON/Arrays/TwoPointers/EasyLevel/1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -1,11 +1,5 @@
-SELECT 
-    FORMAT(trans_date, 'yyyy-MM') AS month,
-    country,
-    COUNT(*) AS trans_count,
-    COUNT(CASE WHEN state = 'approved' THEN 1 END) AS approved_count,
-    SUM(amount) AS trans_total_amount,
-    SUM(CASE WHEN state = 'approved' THEN amount else 0 END) AS approved_total_amount
-FROM transactions
-GROUP BY FORMAT(trans_date, 'yyyy-MM'), country
-ORDER BY month DESC, country desc;
-
+/* Write your T-SQL query statement below */
+select FORMAT(trans_date,'yyyy-MM') month,country,count(*) as trans_count,count( case when state='approved' then 1  end)as approved_count,sum(amount)as trans_total_amount ,sum(case when state='approved' then amount else 0 end)as approved_total_amount
+from Transactions
+group by FORMAT(trans_date,'yyyy-MM'),country
+order by country desc
