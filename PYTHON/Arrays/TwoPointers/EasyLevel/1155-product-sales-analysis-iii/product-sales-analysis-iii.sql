7572fda2e8508b4804c4dfcc93209ec95fd11a7a
Jayanthi Sashank <sashihankler19@gmail.com>
2025-08-14 12:56:32 +0530

    Time: 3664 ms (5.06%) | Memory: 0B (100.00%) - LeetSync

--- PYTHON/Arrays/TwoPointers/EasyLevel/1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ PYTHON/Arrays/TwoPointers/EasyLevel/1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,7 +1,7 @@
 /* Write your T-SQL query statement below */
 with cte as(
-select product_id,year as first_year ,quantity,price,dense_rank()over( partition by product_id order by year)as rank
-from sales
+select product_id,year as first_year, dense_rank() over( partition by product_id order by year) as rank,quantity, price
+from Sales
 )
-select product_id,first_year,quantity ,price from cte where rank=1
-order by product_id,first_year
+select product_id, first_year,quantity,price from cte 
+where rank=1
