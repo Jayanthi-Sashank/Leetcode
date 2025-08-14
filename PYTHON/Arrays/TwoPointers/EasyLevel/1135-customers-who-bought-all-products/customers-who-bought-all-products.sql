e2997d3c5b45bf27cfd3d8fc29a0b168108da804
Jayanthi Sashank <sashihankler19@gmail.com>
2025-08-14 14:37:38 +0530

    Time: 1807 ms (12.34%) | Memory: 0B (100.00%) - LeetSync

--- PYTHON/Arrays/TwoPointers/EasyLevel/1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ PYTHON/Arrays/TwoPointers/EasyLevel/1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,8 +1,7 @@
 /* Write your T-SQL query statement below */
-
-select c.customer_id
-from customer c
-left join product p
-on c.product_key=p.product_key
-group by c.customer_id
-having count(distinct c.product_key) in (select count(*) from product p)
+select customer_id
+from Customer C
+join Product P
+on C.product_key=P.product_key
+group by customer_id
+having count(distinct C.product_key) in (select count(*) from product )
