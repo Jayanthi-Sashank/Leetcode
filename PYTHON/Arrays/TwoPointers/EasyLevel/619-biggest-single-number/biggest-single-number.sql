ba64f1fe7fa62da4de355fd7b996977ffa02fb83
Jayanthi Sashank <sashihankler19@gmail.com>
2025-08-14 12:50:26 +0530

    Time: 368 ms (37.35%) | Memory: 0B (100.00%) - LeetSync

--- PYTHON/Arrays/TwoPointers/EasyLevel/619-biggest-single-number/biggest-single-number.sql
+++ PYTHON/Arrays/TwoPointers/EasyLevel/619-biggest-single-number/biggest-single-number.sql
@@ -1,9 +1,5 @@
 /* Write your T-SQL query statement below */
-select max(num) as num
+select max(num)as num from MyNumbers where num in( select max(num)
 from MyNumbers
-where num not in (
-    select num
-    from MyNumbers
-    group by num
-    having count(*)>1
-)
+group by num
+having count(*)=1)
