9b237ce537dba511cea7246db7af99622d44a358
Jayanthi Sashank <sashihankler19@gmail.com>
2025-08-14 13:08:24 +0530

    Time: 940 ms (11.91%) | Memory: 0B (100.00%) - LeetSync

--- PYTHON/Arrays/TwoPointers/EasyLevel/1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ PYTHON/Arrays/TwoPointers/EasyLevel/1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,7 +1,7 @@
-/* Write your T-SQL query statement below */
-with cte as(
-select product_id,year as first_year, dense_rank() over( partition by product_id order by year) as rank,quantity, price
+# Write your MySQL query statement below
+select product_id,year as first_year,quantity,price 
 from Sales
-)
-select product_id, first_year,quantity,price from cte 
-where rank=1
+where (product_id,year) in ( select product_id, min(year)  from sales group by product_id )
+
+
+
