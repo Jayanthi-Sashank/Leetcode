70eb0ca29f04f2343cce868fb06596366a8bd7fb
Jayanthi Sashank <sashihankler19@gmail.com>
2025-08-14 16:17:25 +0530

    Time: 523 ms (37.85%) | Memory: 0B (100.00%) - LeetSync

--- PYTHON/Arrays/TwoPointers/EasyLevel/1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ PYTHON/Arrays/TwoPointers/EasyLevel/1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,9 +1,15 @@
-# Write your MySQL query statement below
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag='Y' OR 
-    employee_id in
-    (SELECT employee_id
-    FROM Employee
-    Group by employee_id
-    having count(employee_id)=1)
+/* Write your T-SQL query statement below */
+with cte as (
+select employee_id,primary_flag,department_id,count(employee_id) over(partition by employee_id order by employee_id)as counting
+from employee 
+),
+cte1 as(
+select employee_id,
+case when primary_flag='Y' or counting=1 then department_id end as department_id
+from cte)
+select employee_id, department_id
+from cte1
+where department_id is not null
+
+
+
