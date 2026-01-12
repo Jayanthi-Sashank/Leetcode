a1d38132215546237943ddf70d4b6d475a7b2927
Jayanthi Sashank <sashihankler19@gmail.com>
2026-01-12 14:53:09 +0530

    Time: 503 ms (22.99%) | Memory: 0B (100.00%) - LeetSync

--- PYTHON/Arrays/TwoPointers/EasyLevel/197-rising-temperature/rising-temperature.sql
+++ PYTHON/Arrays/TwoPointers/EasyLevel/197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,6 @@
-/* Write your T-SQL query statement below */
-select W2.id
-from Weather W1
-join Weather W2
-on W2.temperature>W1.temperature and datediff(DD,W1.recordDate,W2.recordDate)=1
+# Write your MySQL query statement below
+select curr.id
+from Weather curr
+join Weather prev
+on  datediff(curr.recordDate,prev.RecordDate)=1
+and curr.temperature>prev.temperature
