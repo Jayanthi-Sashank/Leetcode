725807b7fc459c082eaf77e4ef54ebede98cc472
Jayanthi Sashank <sashihankler19@gmail.com>
2026-03-09 10:44:41 +0530

    Time: 619 ms (68.98%) | Memory: 0B (100.00%) - LeetSync

--- PYTHON/Arrays/TwoPointers/EasyLevel/1811-fix-names-in-a-table/fix-names-in-a-table.sql
+++ PYTHON/Arrays/TwoPointers/EasyLevel/1811-fix-names-in-a-table/fix-names-in-a-table.sql
@@ -1,4 +1,3 @@
-/* Write your T-SQL query statement below */
-select user_id,upper(left(name,1))+lower(substring(name,2,len(name))) as name
-from users
-order by user_id
+select user_id,concat(upper(substring(name,1,1)),lower(substring(name,2))) as name
+from Users
+order by user_id asc
